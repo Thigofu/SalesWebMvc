91cc949709fe7c43830c90282b568b552073562c
Thiago Zaleski Leon <thigofu@gmail.com>
2020-11-06 03:41:08 -0300

    Realizado 2°a migração e adicionado novas entidades

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,26 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inicial, final)); // Expressão lambda para somar dos vendedores de tal departamento
+        }
     }
 }
