67ea4ccf3f290257f9b987a356ece1936fb51c22
Thiago Zaleski Leon <thigofu@gmail.com>
2020-11-07 00:33:48 -0300

    Adicionado tratamentos de exceções referente ao Delete dos Sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -69,8 +69,14 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
+            try { 
             await _sellerService.RemoveAsync(id);
             return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Cannot Delete Seller because he/she has sales" });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
